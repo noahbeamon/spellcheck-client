ee4d8ed5cf01fe0d8007341fa597a61d74309286
Noah <noah.beamon648@gmail.com>
2021-11-01 02:47:38 -0400

    support multiple screen sizes

--- src/components/pages/Customdictionary.js
+++ src/components/pages/Customdictionary.js
@@ -138,130 +138,132 @@ function CustomDictionary() {
   };
 
   return (
-    <div style={{ marginTop: 20 }}>
-      <Container>
-        <h1 style={{ color: "#7d8782" }}>
-          Add custom words and images with USB <i class="fab fa-usb"></i>
-        </h1>
-        <form className={classes.root} onSubmit={handleSubmit}>
-          {inputFields.map((inputField) => (
-            <div key={inputField.id}>
-              <TextField
-                name="word"
-                label="word"
-                variant="filled"
-                inputProps={{ maxLength: 5 }}
-                onKeyPress={(e) => {
-                  // if (e.key === " " || e.key === "-" || ) {
-                  //   e.preventDefault();
-                  // }
-                  if (notAllowed.includes(e.key)) {
-                    e.preventDefault();
-                  }
-                }}
-                value={inputField.word}
-                onChange={(event) => {
-                  handleChangeInput(inputField.id, event);
-                }}
-              />
-              <input
-                name="file"
-                type="file"
-                accept="image/png, image/jpeg"
-                value={inputField.file}
-                onChange={(event) => {
-                  handleChangeInput(inputField.id, event);
-
-                  var file = event.target.files[0];
-
-                  var fileInput = false;
-                  if (event.target.files[0]) {
-                    fileInput = true;
-                  }
-                  //for removing image on same index
-                  var found = false;
-                  for (var i = 0; i < images.length; i++) {
-                    if (images[i].id == inputField.id) {
-                      images.splice(i, 1);
-                      found = true;
-                      break;
+    <div>
+      <div style={{ marginTop: 20, minHeight: "100vh" }}>
+        <Container>
+          <h1 style={{ color: "#7d8782" }}>
+            Add custom words and images with USB <i class="fab fa-usb"></i>
+          </h1>
+          <form className={classes.root} onSubmit={handleSubmit}>
+            {inputFields.map((inputField) => (
+              <div key={inputField.id}>
+                <TextField
+                  name="word"
+                  label="word"
+                  variant="filled"
+                  inputProps={{ maxLength: 5 }}
+                  onKeyPress={(e) => {
+                    // if (e.key === " " || e.key === "-" || ) {
+                    //   e.preventDefault();
+                    // }
+                    if (notAllowed.includes(e.key)) {
+                      e.preventDefault();
                     }
-                  }
-                  //
-                  if (fileInput) {
-                    try {
-                      Resizer.imageFileResizer(
-                        event.target.files[0],
-                        320,
-                        240,
-                        "JPEG",
-                        100,
-                        0,
-                        (uri) => {
-                          setImages([
-                            ...images,
-                            { id: inputField.id, file: uri },
-                          ]);
-                        },
-                        "base64"
-                      );
-                    } catch (err) {
-                      console.log(err);
+                  }}
+                  value={inputField.word}
+                  onChange={(event) => {
+                    handleChangeInput(inputField.id, event);
+                  }}
+                />
+                <input
+                  name="file"
+                  type="file"
+                  accept="image/png, image/jpeg"
+                  value={inputField.file}
+                  onChange={(event) => {
+                    handleChangeInput(inputField.id, event);
+
+                    var file = event.target.files[0];
+
+                    var fileInput = false;
+                    if (event.target.files[0]) {
+                      fileInput = true;
                     }
-                  }
-                  // setImages([
-                  //   ...images,
-                  //   { id: inputField.id, file: binary_of_image },
-                  // ]);
+                    //for removing image on same index
+                    var found = false;
+                    for (var i = 0; i < images.length; i++) {
+                      if (images[i].id == inputField.id) {
+                        images.splice(i, 1);
+                        found = true;
+                        break;
+                      }
+                    }
+                    //
+                    if (fileInput) {
+                      try {
+                        Resizer.imageFileResizer(
+                          event.target.files[0],
+                          320,
+                          240,
+                          "JPEG",
+                          100,
+                          0,
+                          (uri) => {
+                            setImages([
+                              ...images,
+                              { id: inputField.id, file: uri },
+                            ]);
+                          },
+                          "base64"
+                        );
+                      } catch (err) {
+                        console.log(err);
+                      }
+                    }
+                    // setImages([
+                    //   ...images,
+                    //   { id: inputField.id, file: binary_of_image },
+                    // ]);
 
-                  // var reader = new FileReader();
+                    // var reader = new FileReader();
 
-                  // reader.onload = function (e) {
-                  //   // The file's text will be printed here
-                  //   //alert(e.target.result);
-                  //   var the_file = resizeImage(e.target.result, 320, 240);
-                  //   setImages([
-                  //     ...images,
-                  //     { id: inputField.id, file: the_file },
-                  //   ]);
-                  //   //alert(JSON.stringify(images));
-                  // };
+                    // reader.onload = function (e) {
+                    //   // The file's text will be printed here
+                    //   //alert(e.target.result);
+                    //   var the_file = resizeImage(e.target.result, 320, 240);
+                    //   setImages([
+                    //     ...images,
+                    //     { id: inputField.id, file: the_file },
+                    //   ]);
+                    //   //alert(JSON.stringify(images));
+                    // };
 
-                  // reader.onload = function (e) {
-                  //   // The file's text will be printed here
-                  //   //alert(e.target.result);
-                  //   setImages([
-                  //     ...images,
-                  //     { id: inputField.id, file: e.target.result },
-                  //   ]);
-                  //   //alert(JSON.stringify(images));
-                  // };
-                  //reader.readAsBinaryString(file);
-                }}
-              />
-              <IconButton
-                disabled={inputFields.length === 1}
-                onClick={() => handleRemoveFields(inputField.id)}
-              >
-                <i class="fa fa-minus"></i>
-              </IconButton>
-              <IconButton onClick={handleAddFields}>
-                <i class="fa fa-plus"></i>
-              </IconButton>
-            </div>
-          ))}
-          <Button
-            style={{ marginBottom: 20 }}
-            role="button"
-            onClick={handleSubmit}
-          >
-            Send Words
-          </Button>
-        </form>
-        <p>Images Added: {images.length}</p>
-        <p>Image Array: {JSON.stringify(images)}</p>
-        <p>Word Array: {JSON.stringify(inputFields)}</p>
-      </Container>
+                    // reader.onload = function (e) {
+                    //   // The file's text will be printed here
+                    //   //alert(e.target.result);
+                    //   setImages([
+                    //     ...images,
+                    //     { id: inputField.id, file: e.target.result },
+                    //   ]);
+                    //   //alert(JSON.stringify(images));
+                    // };
+                    //reader.readAsBinaryString(file);
+                  }}
+                />
+                <IconButton
+                  disabled={inputFields.length === 1}
+                  onClick={() => handleRemoveFields(inputField.id)}
+                >
+                  <i class="fa fa-minus"></i>
+                </IconButton>
+                <IconButton onClick={handleAddFields}>
+                  <i class="fa fa-plus"></i>
+                </IconButton>
+              </div>
+            ))}
+            <Button
+              style={{ marginBottom: 20 }}
+              role="button"
+              onClick={handleSubmit}
+            >
+              Send Words
+            </Button>
+          </form>
+          <p>Images Added: {images.length}</p>
+          <p>Image Array: {JSON.stringify(images)}</p>
+          <p>Word Array: {JSON.stringify(inputFields)}</p>
+        </Container>
+      </div>
       <Footer />
     </div>
   );
